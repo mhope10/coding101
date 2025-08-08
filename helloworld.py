f3fcbdb5a3222a995707b5afb02fd01f6744a067
Maxi Esperanza <maximiliano.esperanza@gmail.com>
2025-08-08 13:10:41 -0300

    "adding hello world"

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+print("hello world")
+# This script prints "hello world" to the console.
+# It is a simple demonstration of a Python script.
+# You can run this script using Python 3 interpreter.
+# Save this code in a file named hello.py and run it using the command:
+# python3 hello.py
+# Make sure you have Python 3 installed on your system.
+# The script does not take any input and does not return any output.
+# It is intended for educational purposes to show how to print text in Python.
+
+
+
+
